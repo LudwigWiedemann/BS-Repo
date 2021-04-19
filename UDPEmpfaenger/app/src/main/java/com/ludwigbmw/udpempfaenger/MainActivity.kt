955718b62d0e5203d66b443508f391cf526f140d
Ludwig Wiedemann <ludwigwwiedemann@gmail.com>
2021-04-19 16:40:19 +0200

    neuer empfaenger und sender

--- UDPEmpfaenger/app/src/main/java/com/ludwigbmw/udpempfaenger/MainActivity.kt
+++ UDPEmpfaenger/app/src/main/java/com/ludwigbmw/udpempfaenger/MainActivity.kt
@@ -1,57 +1,37 @@
 package com.ludwigbmw.udpempfaenger
 
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
+import android.util.Log
+import java.lang.Exception
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
-import java.nio.file.Files.find
 
-open class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    class UDPEmpfaenger : Runnable {
+        override fun run() {
+            var socket: DatagramSocket? = null
+            while (true) {
+                try {
+                    socket = DatagramSocket(5004,
+                            InetAddress.getByName("192.168.2.19"))
+                    socket.broadcast = true
+                    val buffer = ByteArray(2048)
+                    val packet = DatagramPacket(buffer, buffer.size)
+                    socket.receive(packet)
+                    Log.d("hjhj", "Packet received")
+                } catch (e: Exception) {
+                    Log.d("jj", "jk", e)
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Thread(UdpDataArrival()).start()
-       }
-
-    private var updateUIHandler = object: Handler(Looper.getMainLooper()) {
-        override fun handleMessage(msg: Message) {
-            this@MainActivity.runOnUiThread {
-                tv_message.text = msg.data?.getString("message_content")
-            }
-        }
+        Thread(UDPEmpfaenger()).start()
     }
 
-    class UdpDataArrival: Runnable {
-        override fun run() {
-            Log.d("LogTag", "Thread started")
-            while (true) {
-                val buffer = ByteArray(2048)
-                var socket: DatagramSocket? = null
-                try {
-                    val locIP = InetAddress.getByName("192.168.2.19")
-                    socket = DatagramSocket(5004, locIP)
-                    socket.broadcast = true
-                    val packet = DatagramPacket(buffer, buffer.size)
-                    socket.receive(packet)
-                    var msg = "String(packet.data)"
-                    val message = Message.obtain()
-                    message.data.putString("message_content", msg)
-                    // TODO: find a way to use the UI Handler in this class
-                    Log.e("LogTag", msg)
-                } catch (e: Exception) {
-                    Log.e("LogTag", "error", e)
-                } finally {
-                    socket?.close()
-                }
-            }
-
-        }
-
-    }
 }
